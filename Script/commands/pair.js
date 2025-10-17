f439b99374baffd0fd34d70c00c57cdfb1e05539
Rahat Islam <rahatbott10@gmail.com>
2025-10-18 00:35:08 +0600

    Update pair.js

--- Script/commands/pair.js
+++ Script/commands/pair.js
@@ -4,7 +4,7 @@ module.exports.config = {
  hasPermssion: 0,
  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
  description: "Pair two users with a fun compatibility score",
- commandCategory: "Picture",
+ commandCategory: "🩵love🩵",
  cooldowns: 5,
  dependencies: {
  "axios": "",
