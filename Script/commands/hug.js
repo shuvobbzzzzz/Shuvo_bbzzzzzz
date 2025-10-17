79fc78850a378fe76331554f85bd156c7af5e4d7
Rahat Islam <rahatbott10@gmail.com>
2025-10-18 00:46:33 +0600

    Update hug.js

--- Script/commands/hug.js
+++ Script/commands/hug.js
@@ -4,7 +4,7 @@ module.exports.config = {
  hasPermssion: 0,
  credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
  description: "hug frame create",
- commandCategory: "img",
+ commandCategory: "🩵love🩵",
  usages: "[@mention]",
  cooldowns: 5,
  dependencies: {
